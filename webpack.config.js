ba978369c8cdfa4f6a301f7505a2ae7007c3dcaa
jelilking <abduljelilozigi@gmail.com>
2024-05-31 23:19:50 +0100

    my boiler web pack template

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+    publicPath: "/assets/", // Move publicPath here
+  },
+  devServer: {
+    static: path.resolve(__dirname, "dist"), // Update contentBase to static
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
